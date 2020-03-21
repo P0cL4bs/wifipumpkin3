e0c2786694f4ea6b33f8cc5d77a90f1967e141b4
mh4x0f <mh4root@gmail.com>
2020-03-21 20:57:08 -0300

    removed somes import nyscreen

--- wifipumpkin3/core/main.py
+++ wifipumpkin3/core/main.py
@@ -5,7 +5,6 @@ from wifipumpkin3.core.utility.collection import SettingsINI
 import wifipumpkin3.core.utility.constants  as C
 from wifipumpkin3.core.utility.printer import display_messages,setcolor
 from termcolor import colored
-import npyscreen, threading
 from tabulate import tabulate
 
 from wifipumpkin3.core.common.defaultwidget import *
@@ -23,29 +22,6 @@ from wifipumpkin3.modules import *
 from wifipumpkin3.modules import module_list, all_modules
 
 approot = QtCore.QCoreApplication.instance()
-
-class TestApp(npyscreen.NPSApp):
-    def main(self):
-        # These lines create the form and populate it with widgets.
-        # A fairly complex screen in only 8 or so lines of code - a line for each control.
-        F  = npyscreen.Form(name = "Welcome to Npyscreen",)
-        t  = F.add(npyscreen.TitleText, name = "Text:",)
-        fn = F.add(npyscreen.TitleFilename, name = "Filename:")
-        fn2 = F.add(npyscreen.TitleFilenameCombo, name="Filename2:")
-        dt = F.add(npyscreen.TitleDateCombo, name = "Date:")
-        s  = F.add(npyscreen.TitleSlider, out_of=12, name = "Slider")
-        ml = F.add(npyscreen.MultiLineEdit,
-               value = """try typing here!\nMutiline text, press ^R to reformat.\n""",
-               max_height=5, rely=9)
-        ms = F.add(npyscreen.TitleSelectOne, max_height=4, value = [1,], name="Pick One",
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
-        ms2= F.add(npyscreen.TitleMultiSelect, max_height =-2, value = [1,], name="Pick Several",
-                values = ["Option1","Option2","Option3"], scroll_exit=True)
-
-        # This lets the user interact with the Form.
-        F.edit()
-
-        print(ms.get_selected_objects())
 
 class PumpkinShell(Qt.QObject, ConsoleUI):
     """
