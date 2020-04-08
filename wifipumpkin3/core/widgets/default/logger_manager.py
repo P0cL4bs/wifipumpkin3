9b3839418367a6f9eb3a81da32f680123b6d77d2
mh4x0f <mh4root@gmail.com>
2020-04-07 23:14:25 -0300

    added license header all files in core/widgets

--- wifipumpkin3/core/widgets/default/logger_manager.py
+++ wifipumpkin3/core/widgets/default/logger_manager.py
@@ -5,11 +5,27 @@ from loguru import logger
 import sys
 import json
 
+# This file is part of the wifipumpkin3 Open Source Project.
+# wifipumpkin3 is licensed under the Apache 2.0.
+
+# Copyright 2020 P0cL4bs Team - Marcos Bomfim (mh4x0f)
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 def make_filter(name):
     def filter(record):
         return record["extra"].get("name") == name
     return filter
-
 
 class StandardLog:
     _typelog  = None
@@ -135,7 +151,6 @@ class StandardLog:
     def debug(self, message):
         self.configure()
         self.logger.debug(message)
-
 
 class LoggerManager(CoreSettings):
     Name = "Logger Manager"
